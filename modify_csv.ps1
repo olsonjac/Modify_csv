9dfc2d24f456fe3ba383b88302c3a6c972ae972c
Jacob Olson <olsonjac17@gmail.com>
2022-08-01 10:20:06 -0400

    Added functionality that converts the csv file into an excel file format

--- modify_csv.ps1
+++ modify_csv.ps1
@@ -1,9 +1,9 @@
 #initial file location using a wild card to pull any csv file present
-$inputfilepath="C:\Users\olson\OneDrive\Documents\Ingest\*.csv"
+$inputfilepath="C:\Users\olson\OneDrive\Documents\Ingest\example.csv"
 #date varianble used to generate unique file names that reference time of creation
 $date = (Get-Date -Format "yyyy.MM.dd_hhmm_tt")
 #new csv import destination directory using that date variable
-$destinationfilepath="C:\Users\olson\OneDrive\Documents\Ingest\temp\new$date"
+$destinationfilepath="C:\Users\olson\OneDrive\Documents\Ingest\archive\new_file$date.xlsx"
 #variable that will hold the csv file that is imported at the filepath location
 $names=Import-Csv -Path $inputfilepath
 
@@ -11,7 +11,7 @@ $names=Import-Csv -Path $inputfilepath
 foreach($name in $names){
     $newname="$($name.AssetName) $($name.Type)"
     
-    Add-Member -InputObject $name -MemberType NoteProperty -Name "newname" -Value $newname
+    Add-Member -InputObject $name -MemberType NoteProperty -Name "Hybrid_column" -Value $newname -Force
 }
-#final export statement that will push the newly modified csv file to the the destination directory
-$names | Export-Csv -Path $destinationfilepath -NoTypeInformation
+#final export statement that will push the newly modified csv file to the the destination directory as an excel file format
+$names | Export-Excel $destinationfilepath
